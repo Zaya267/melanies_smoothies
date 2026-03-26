98f44375b643beaef71151fcf81b5c06c648b344
Zaya267 <108730830+Zaya267@users.noreply.github.com>
2026-03-26 18:52:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,14 +37,14 @@ if ingredients_list:
 
     st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,Smoothie_Name)
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                     values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
     insertdata = st.button('Submit Order')
     if insertdata:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,Smoothie_Name)
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                     values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
 
     #st.write(my_insert_stmt)    
