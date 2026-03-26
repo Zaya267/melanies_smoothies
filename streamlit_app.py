e3f01e5a2c5036b18dbc329bdd05b024c667966b
Zaya267 <108730830+Zaya267@users.noreply.github.com>
2026-03-26 17:41:50 +0200

    Modify dataframe handling in streamlit_app.py
    Comment out previous dataframe display and stop calls, and add conversion to pandas dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,13 +9,16 @@ session = cnx.session()
 st.title(f"Customize YOUR Smoothie :cup_with_straw: {st.__version__}")
 #session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
 import streamlit as st
 import requests
 name_on_order = st.text_input("Name Of Smoothie:")
 st.write("The name of YOUR smoothie will be:", name_on_order)
 
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 ingredients_list = st.multiselect(
     "Choose upto 5 ingredients:"
      ,my_dataframe
